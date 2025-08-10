6345536aaff2b77b6a76d5e66067498548910812
aabh0002 <aabh0002@student.monash.edu>
2025-08-10 12:54:52 +1000

    end points implemented for fetching ownership and parking data

--- Backend/server.py
+++ Backend/server.py
@@ -1,10 +1,33 @@
-from flask import Flask
+from flask import Flask, jsonify
+from sqlalchemy import create_engine, text
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
 
-@app.route('/')
-def root():
-    return "You have reached the backend server!"
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+    connect_args={"sslmode": "require"}
+)
 
-if __name__ == '__main__':
-    app.run(port=5000)
+@app.route("/car_ownership")
+def get_car_ownership():
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT * FROM vic_car_ownership_data"))
+        rows = [dict(row) for row in result.mappings()]
+    return jsonify(rows)
+
+@app.route("/parking")
+def get_parking():
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT * FROM parking_data"))
+        rows = [dict(row) for row in result.mappings()]
+    return jsonify(rows)
+
+
+if __name__ == "__main__":
+    app.run(port = 5000, debug=True)
